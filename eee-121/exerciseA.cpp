b4e961ac7f1f297ea91ec7e06dbe029e6f3a7f75
Nile Jocson <stellanchier@gmail.com>
2026-01-27 13:53:45 +0800

    Remove ho-1.pdf, add exerciseA.cpp

---
+++ eee-121/exerciseA.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+	int number = 0;
+	std::cout << "Enter a single-digit number: ";
+	std::cin >> number;
+
+	if (std::cin.fail())
+	{
+		std::cout << "Your input is not a number.";
+		return EXIT_FAILURE;
+	}
+
+	if (number < -9 || number > 9)
+	{
+		std::cout << "Your number is not a single digit.";
+		return EXIT_FAILURE;
+	}
+
+	if (number % 2 == 0)
+	{
+		std::cout << number << " is even." << '\n';
+	}
+	else
+	{
+		std::cout << number << " is odd." << '\n';
+	}
+}
