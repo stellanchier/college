64ea7a4a6cd6de68f492292b85a76a5702d516ca
Nile Jocson <stellanchier@gmail.com>
2026-01-27 14:38:05 +0800

    Add exerciseB.cpp

---
+++ eee-121/exerciseB.cpp
@@ -0,0 +1,39 @@
+#include <ios>
+#include <iostream>
+#include <limits>
+#include <string>
+
+template<typename T, typename Callable>
+T input_predicate(std::string const &prompt, Callable predicate)
+{
+	T input {};
+	while (true)
+	{
+		std::cout << prompt;
+		std::cin >> input;
+
+		if (std::cin.fail() || !predicate(input))
+		{
+			std::cout << "Invalid input.\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+
+		break;
+	}
+
+	return input;
+}
+
+int main()
+{
+	auto number = input_predicate<int>("Enter a two-digit positive integer: ", [](int const i) -> bool {
+		return i >= 10 && i <= 99;
+	});
+
+	for (int i = 9; i < number; i += 9)
+	{
+		std::cout << i << '\n';
+	}
+}
